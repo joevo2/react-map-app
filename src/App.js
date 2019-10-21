9779ebfdf46cf4e88951ecf4c5005b98c176f03c
Joel Yek Zong Yong <joel.yekzongyong@baesystems.com>
2019-10-21 16:14:30 +0800

    Added mapbox map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import MapGL, { GeolocateControl } from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import styles from './App.module.css';
+
+const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function App() {
+  const [viewport, setViewPort] = useState({
+    width: '100%',
+    height: 900,
+    latitude: 0,
+    longitude: 0,
+    zoom: 2
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        onViewportChange={viewport => setViewPort({ ...viewport })}
+      >
+        <GeolocateControl
+          className={styles.geolocate_button}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+        />
+      </MapGL>
     </div>
   );
 }
